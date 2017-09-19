74fcb05f2ba9b6f1bd44e0e9e9f668dac8ea242a
Wole Obayomi <oluwole.obayomi@mail.mcgill.ca>
2017-09-19 16:31:50 +0000

    setup up auth forms

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app     = express();
+var bcrypt  = require('bcrypt');
 
 var bodyParser = require('body-parser');
 var morgan     = require('morgan');
 
 var PORT       = process.env.PORT || 3000;
+
+//encryption helpers
+app.locals = {
+    encryptPass: function (toEncrypt) {
+        var hash = bcrypt.hashSync(toEncrypt, 10); //10 is salt rounds...whatever that means
+        return hash;
+    },
+    
+    compare: function (notEncrypted, encrypted) {
+        return bcrypt.compareSync(notEncrypted, encrypted);
+    }
+};
 
 app.set('view engine', 'ejs');
 
@@ -27,18 +40,39 @@ app.get('/', function(req, res){
     res.end();
 });
 
+//view handlers
 app.get('/login', function(req, res){
     res.render('login');    
 });
 
 app.get('/signup', function(req, res){
-    res.render('signup');    
+    res.render('signup', {
+        displaySuccess: false
+    });    
 });
 
-app.post('login', function(req, res) {
+//form submission handlers
+app.post('/login', function(req, res) {
     
 });
 
-app.post('signup', function(req, res){
+app.post('/signup', function(req, res){
+    var email    = req.body.email;
+    var password = req.body.password;
+    var confirm  = req.body.confirm;
+    
+    if (email && password && confirm) {
+        res.render('signup', {
+           email: email,
+           password: app.locals.encryptPass(password),
+           confirm: app.locals.compare(confirm, app.locals.encryptPass(password)),
+           displaySuccess: true
+        });    
+    } 
+    else {
+        res.render('signup', {
+            displaySuccess: false
+        });    
+    }
     
 });
