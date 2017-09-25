1498cb23f8877c36bfa140baaca2b1e5755484f3
daddytheparent <oluwole.obayomi@mail.mcgill.ca>
2017-09-25 13:25:27 -0400

    further restructuring

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -19,26 +19,6 @@ router.post('/login', auth.login(), function(req, res) {
     
     res.render(status.destination, { message: status.message });
     
-    /*
-	if(email && password) {
-		var query = {email_address : email};
-		var user = models.login_credentials.find({where: query})
-		.then(function(user) {
-			if (user) {
-				// User exists, compare password
-				bcrypt.compare(password, user.hashed_password, function (err, doesMatch) {
-					if (doesMatch) {
-						res.render('login', {
-							displaySuccess: true,
-							message: 'You kinda logged in successfully'
-						})
-					}
-				});
-			}
-		});
-	}
-    */
-    
 });
 
 router.post('/signup', auth.signup(), function(req, res){
@@ -46,24 +26,6 @@ router.post('/signup', auth.signup(), function(req, res){
     var status = res.locals.status;
     
     res.render(status.destination, { message: status.message });
-    /*
-    if (email && password && confirm) {
-		models.login_credentials.create({email_address: email, hashed_password: app.locals.encryptPass(password)})
-        .then(() => {
-          res.render('signup', {
-           email: email,
-           password: encryptor.encryptPass(password),
-           confirm:  encryptor.compare(confirm, encryptor.encryptPass(password)),
-           displaySuccess: true
-        });
-        });
-    }    
-    else {
-        res.render('signup', {
-            displaySuccess: false
-        });    
-    }
-    */
     
 });
 
--- config.js
+++
@@ -1,7 +0,0 @@
-const nconf = require('nconf');
-const path  = require('path');
-
-nconf.env()
-     .file({file: path.resolve(__dirname, 'config.json'),});
-
-module.exports = nconf
